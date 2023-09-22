f927d3acdc58d10287833e9f30b3bb249ef0f359
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-22 10:52:43 -0300

    Grade, Modelo e BLocks

--- MySliderC/paginas/views.py
+++ MySliderC/paginas/views.py
@@ -3,3 +3,5 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
 
+class GradeView(TemplateView):
+    template_name = "paginas/grade.html"
