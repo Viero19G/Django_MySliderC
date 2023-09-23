26eebaea75334b5cb03faea345e64c07599ecec8
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-23 19:36:58 -0300

    Modificado models, criado modulo para cadastros

--- MySliderC/carrosselApp/models.py
+++ MySliderC/carrosselApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from distutils.command.upload import upload
+from django.forms import CheckboxSelectMultiple 
 # Classes e modelos para a construção de todo o projeto
 # associados ao bando de dados com os comandos _py manage.py makemigrations
 # e py manage.py migrate _ após esses comandos os models são incorporados ao
@@ -14,36 +15,38 @@ class Conteudo(models.Model):
     tempo = models.PositiveBigIntegerField()
 
     def __str__(self):
-        return "{} ({}) ".format(self.title, self.image)
+        return "{} ({}) ".format(self.title, self.sub_title)
 
 class Grade(models.Model):
-    conteudo = models.ForeignKey(Conteudo, on_delete=models.PROTECT)
+    conteudo = models.ManyToManyField(Conteudo, blank=True)  # Use ManyToManyField para permitir múltiplas seleções
     title = models.CharField(max_length=150)
     sub_title = models.CharField(max_length=200)
-    def __str__(self):
-        return self.title
 
-class Setor(models.Model):
-    tela = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} ({})".format(self.title, ', '.join([str(c) for c in self.conteudo.all()]))
+
+class Setor(models.Model): 
+    grade = models.ForeignKey(Grade, on_delete=models.PROTECT, default=None)
     nome=models.CharField(max_length=180)
     def __str__(self):
-        return self.nome
+        return "{} ({}) ".format(self.nome, self.grade.title)
 
 class Usuario(models.Model):
     usrNome=models.CharField(max_length=100)
     usrSenha=models.CharField(max_length=100)
-    usrMail=models.CharField(max_length=100)
+    usrMail=models.EmailField(unique=True)  # 'unique=True' garante que cada e-mail seja único no banco de dados
+    # Outros campos do usuário
     setor = models.ForeignKey(Setor, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.usrNome
+        return "{} ({}) ".format(self.usrNome, self.setor.nome)
     
 class Perfil(models.Model):
     perfilNome=models.CharField(max_length=100)
     descricao=models.CharField(max_length=300)
 
     def __str__(self):
-        return self.perfilNome
+        return "{} ({}) ".format(self.perfilNome, self.descricao)
     
 
 class Perfil_Usuario(models.Model):
@@ -51,5 +54,4 @@ class Perfil_Usuario(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
     perfil = models.ForeignKey(Perfil, on_delete=models.PROTECT)
     def __str__(self):
-        return self.descricao
-    
+        return "{} ({}) ({})".format(self.descricao, self.usuario.usrNome, self.perfil.perfilNome)
