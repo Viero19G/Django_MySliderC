0e27641c22659b954b2b75927087ca4e508da24d
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-24 13:08:22 -0300

    Navegacao e upviews para update

---
+++ MySliderC/cadastros/upviews.py
@@ -0,0 +1,39 @@
+from django.views.generic.edit import UpdateView   ##### views para update
+from carrosselApp.models import *
+from django.urls import reverse_lazy
+
+class SetorUpdate(UpdateView):
+    model = Setor
+    fields = ['grade', 'nome']
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
+
+class GradeUpdate(UpdateView):
+    model = Grade
+    fields = [ 'title','sub_title', 'conteudo']
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
+
+class ConteudoUpdate(UpdateView):
+    model = Conteudo
+    fields = [ 'title','sub_title','descricao','tempo', 'image']
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
+
+class UsuarioUpdate(UpdateView):
+    model = Usuario
+    fields = [ 'usrNome','usrSenha', 'usrMail']
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
+
+class PerfilUpdate(UpdateView):
+    model = Perfil
+    fields = [ 'perfilNome','descricao',]
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
+
+class Perfil_UsuarioUpdate(UpdateView):
+    model = Perfil_Usuario
+    fields = [ 'descricao','usuario','perfil']
+    template_name = 'cadastros/create.html'
+    success_url = reverse_lazy('inicio')
--- MySliderC/cadastros/urls.py
+++ MySliderC/cadastros/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
+### importando views para create
 from .views import *
+### importando views de update
+from .upviews import *
+
+
 
 urlpatterns = [
+    #### urls para create ####
     path('setor/',SetorCreate.as_view(), name='cadastrar-setor'),
     path('grade/',GradeCreate.as_view(), name='cadastrar-grade'),
     path('perfil/',PerfilCreate.as_view(), name='cadastrar-perfil'),
@@ -10,4 +15,13 @@ urlpatterns = [
     path('perfil-usuario/',Perfil_UsuarioCreate.as_view(), name='cadastrar-perfil-usuario'),
     path('conteudo/',ConteudoCreate.as_view(), name='cadastrar-conteudo'),
  
+
+ #### urls para update ####
+
+    path('editar/setor/<int:pk>/', SetorUpdate.as_view(), name='upSetor'),
+    path('editar/grade/<int:pk>/', GradeUpdate.as_view(), name='upGrade'),
+    path('editar/perfil/<int:pk>/', PerfilUpdate.as_view(), name='upPerfil'),
+    path('editar/usuario/<int:pk>/', UsuarioUpdate.as_view(), name='upUsuario'),
+    path('editar/perfil-usuario/<int:pk>/', Perfil_UsuarioUpdate.as_view(), name='upUsuarioPerfil'),
+    path('editar/conteudo/<int:pk>/', ConteudoUpdate.as_view(), name='upConteudo'),
 ]
