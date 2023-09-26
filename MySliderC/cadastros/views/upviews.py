121c7645ad8e54a1db4767cdae7fa2129be7a4db
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-26 16:42:07 -0300

    Ajustes para mensagem quando o acesso não é permitido

--- MySliderC/cadastros/views/upviews.py
+++ MySliderC/cadastros/views/upviews.py
@@ -5,45 +5,39 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import GroupRequiredMixin
 from django.http import HttpResponseForbidden
 
-class SetorUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+class SetorUpdate(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
-    group_required = [u"marketing", u"administrador"]
     model = Setor
     fields = ['grade', 'nome']
     template_name = 'cadastros/create.html'
     success_url = reverse_lazy('listSetor')
 
     def dispatch(self, request, *args, **kwargs):
-        # Obtenha o objeto Setor que deseja atualizar
         setor = self.get_object()
 
         # Verifique se o usuário logado é o mesmo que criou o Setor
         if setor.usuario == request.user:
             return super().dispatch(request, *args, **kwargs)
         else:
-            # Redirecione para alguma página de erro ou exiba uma mensagem de erro
             return HttpResponseForbidden("Você não tem permissão para editar este Setor.")
 
-class GradeUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+class GradeUpdate(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
-    group_required = [u"marketing", u"administrador"]
     model = Grade
     fields = [ 'title','sub_title', 'conteudo']
     template_name = 'cadastros/create.html'
     success_url = reverse_lazy('listGrade')
 
     def dispatch(self, request, *args, **kwargs):
-        # Obtenha o objeto Grade que deseja atualizar
         grade = self.get_object()
 
         # Verifique se o usuário logado é o mesmo que criou a Grade
         if grade.usuario == request.user:
             return super().dispatch(request, *args, **kwargs)
         else:
-            # Redirecione para alguma página de erro ou exiba uma mensagem de erro
             return HttpResponseForbidden("Você não tem permissão para editar esta Grade.")
 
-class ConteudoUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+class ConteudoUpdate(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
     model = Conteudo
     fields = [ 'title','sub_title','descricao','tempo', 'image']
@@ -51,14 +45,12 @@ class ConteudoUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('listConteudo')
 
     def dispatch(self, request, *args, **kwargs):
-        # Obtenha o objeto Conteúdo que deseja atualizar
         conteudo = self.get_object()
 
         # Verifique se o usuário logado é o mesmo que criou o Conteúdo
         if conteudo.usuario == request.user:
             return super().dispatch(request, *args, **kwargs)
         else:
-            # Redirecione para alguma página de erro ou exiba uma mensagem de erro
             return HttpResponseForbidden("Você não tem permissão para editar este Conteúdo.")
 
 
