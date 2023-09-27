38485d4d47e3ce2de80fcb744ae2e64464d60c91
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-27 15:28:46 -0300

    email no repeat

--- MySliderC/usuarios/forms.py
+++ MySliderC/usuarios/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
 
 class UsuarioForm(UserCreationForm):
     email = forms.EmailField(max_length=150)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password1']
+        fields = ['username', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists(): # verifica se é verdadeiro
+            raise ValidationError("O email {} informado já está em uso.".format(email)) # se for aparece a mensagem de erro
+        return email # sem o return função não funciona
--- MySliderC/usuarios/views.py
+++ MySliderC/usuarios/views.py
@@ -1,14 +1,26 @@
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
-
+from django.shortcuts import get_object_or_404
 
 
 class UsuarioCreate(CreateView):
     template_name = "cadastros/create.html"
     form_class = UsuarioForm
     success_url= reverse_lazy('login')
+
+    def form_valid(self, form):
+
+        grupo = get_object_or_404(Group, name='usuario') # busca grupo
+
+        url = super().form_valid(form)
+
+        self.object.groups.add(grupo) # pega objeto e define grupo
+        self.object.save() # salva objeto com novo grupo antes de retornar
+
+        return url
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
 
