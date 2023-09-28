c90f595a60450c8317b2fe8b38e6825580ce01eb
Gabriel Viero <gabrielviero22@gmail.com>
2023-09-28 09:29:12 -0300

    ajustes básicos concluídos

--- MySliderC/usuarios/models.py
+++ MySliderC/usuarios/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Perfil(models.Model):
-    nome_completo= models.CharField(max_length=50, null=True)
-    cpf = models.CharField(max_length=14, null=True)
-    telefone = models.CharField(max_length=16, null=True)
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome_completo= models.CharField(max_length=50, null=True, verbose_name='Nome Completo ')
+    cpf = models.CharField(max_length=14, null=True, verbose_name='CPF ')
+    telefone = models.CharField(max_length=16, null=True, verbose_name='Telefone ')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário ')
