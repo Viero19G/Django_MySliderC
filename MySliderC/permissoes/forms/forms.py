e2f39918064ea01733125bd3e257d526fcd922ac
Gabriel Viero <gabrielviero22@gmail.com>
2023-10-23 21:23:43 -0300

    Atualizar .gitignore

--- MySliderC/permissoes/forms/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from django.contrib.auth.models import Group
-
-class GrupoForm(forms.ModelForm):
-    class Meta:
-        model = Group
-        fields = ['name']
--- MySliderC/permissoes/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from permissoes.views import ManageGroupsView
-
-urlpatterns = [
-    path('controleGrupos/', ManageGroupsView.as_view(), name='controleGrupos'),
-]
